c8f255b9b33e8b4212831d3296b89f9d39b46245
Zuolar <yam8511@gmail.com>
2020-09-27 17:20:07 +0800

    add home link

--- show_auth.php
+++ show_auth.php
@@ -1,4 +1,5 @@
 <div style="text-align: right;">
     <span><?= "{$_auth['name']} [" . ($_auth['job'] == 'family' ? '家屬' : '看護') . "]" ?></span>
+    <a href="/">首頁</a>
     <a href="/logout.php">登出</a>
 </div>
